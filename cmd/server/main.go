f07b56941692729f9a70f02268f71981b8bceef7
prathish-ks <sriyapaka@gmail.com>
2025-07-19 06:38:47 +1000

    main file

---
+++ cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/prathish-ks/openbank-connect/internal/handler"
+)
+
+func main() {
+    r := chi.NewRouter()
+
+    r.Get("/health", handler.HealthCheck)
+
+    log.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", r); err != nil {
+        log.Fatal(err)
+    }
+}
